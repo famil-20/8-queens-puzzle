8cb6e641026d5735485ecd3ab45b97c2e9d09096
Famil <111319058+famil-20@users.noreply.github.com>
2022-08-21 18:31:46 +0400

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int size;
         Scanner scanner = new Scanner(System.in);
         System.out.println("~~~~~~~~~Queen problem~~~~~~~~~");
